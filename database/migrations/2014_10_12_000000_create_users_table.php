d8eae0021b240890719ca7befc0636af5f20cec9
vinzzz001 <vinzzz001@gmail.com>
2022-10-18 15:59:05 +0200

    Clean tickets install.
    -clean install
    -updated

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -8,17 +8,20 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
-     *
+     * todo: Hash password
      * @return void
      */
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('first_name');
+            $table->string('last_name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolval('is_admin');
+            $table->int('telephone_number');
             $table->rememberToken();
             $table->timestamps();
         });
--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,3 +1,7 @@
+<script setup>
+
+</script>
+
 <template>
    <h1>Laravel 9 & Vue3</h1>
 </template>
